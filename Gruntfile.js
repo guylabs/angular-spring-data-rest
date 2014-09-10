abdc1cbf03f0f17e5188d1cd02d7aa9d73ef7a79
guylabs <admin@guylabs.org>
2014-09-10 21:33:21 +0200

    Wrap the concated files in a function scope..

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,11 @@
 module.exports = function (grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON("package.json"),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
         concat: {
             options: {
+                banner: "(function() {\n\n'use strict';\n\n",
+                footer: '\n})();',
                 separator: '\n',
                 process: true
             },
@@ -12,40 +14,40 @@ module.exports = function (grunt) {
                     'src/angular-spring-data-rest-module.js',
                     'src/angular-spring-data-rest-provider.js',
                     'src/angular-spring-data-rest-interceptor-provider.js',
-                    'src/angular-spring-data-rest-utils.js',
+                    'src/angular-spring-data-rest-utils.js'
                 ],
                 dest: 'dist/<%= pkg.name %>.js'
             }
         },
-		uglify: {
-			options: {
+        uglify: {
+            options: {
                 banner: "/*!\n * <%= pkg.name %> <%= pkg.version %>\n * Copyright <%= grunt.template.today('yyyy') %> Guy Brûlé (@guy_labs)\n * https://github.com/guylabs/angular-spring-data-rest\n */\n"
-			},
+            },
             dist: {
                 files: {
                     'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
                 }
             }
-		},
-		karma: {
-			unit: {
-				configFile: "karma.conf.js"
-			},
-			continuous: {
-				configFile: "karma.conf.js",
-				singleRun: true
-			}
-		}
-	});
+        },
+        karma: {
+            unit: {
+                configFile: "karma.conf.js"
+            },
+            continuous: {
+                configFile: "karma.conf.js",
+                singleRun: true
+            }
+        }
+    });
 
     grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-karma");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-karma");
 
     grunt.registerTask("default", ["karma:continuous", "concat", "uglify"]);
 
-	grunt.registerTask("startTestServer", ["karma:unit:start"]);
-	grunt.registerTask("runTests", ["karma:unit:run"]);
-	grunt.registerTask("test", ["karma:continuous"]);
+    grunt.registerTask("startTestServer", ["karma:unit:start"]);
+    grunt.registerTask("runTests", ["karma:unit:run"]);
+    grunt.registerTask("test", ["karma:continuous"]);
 
 };
