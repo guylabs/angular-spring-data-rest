3b823e99bdffc9546ab73870e3c3401b8cb356ca
Anthony Howell <anthonyrhowell@gmail.com>
2015-08-08 16:27:51 -0500

    interceptor will only call process() on response if response.data is object

--- src/angular-spring-data-rest-interceptor-provider.js
+++ src/angular-spring-data-rest-interceptor-provider.js
@@ -17,6 +17,9 @@ angular.module("spring-data-rest").provider("SpringDataRestInterceptor",
 
                     return {
                         response: function (response) {
+                            if(!angular.isObject(response.data)){
+                                return response;
+                            }
                             return SpringDataRestAdapter.process(response.data).then(function (processedResponse) {
                                 response.data = processedResponse;
                                 return response;
